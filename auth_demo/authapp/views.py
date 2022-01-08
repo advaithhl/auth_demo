dd0d64002b7484a7ba829a362484a2efc5b49767
Advaith H L <niceadvaith@gmail.com>
2022-01-08 15:55:58 +0530

    Use template inheritance

--- auth_demo/authapp/views.py
+++ auth_demo/authapp/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, 'authapp/index.html')
+def profile(request):
+    return render(request, 'authapp/profile.html')
