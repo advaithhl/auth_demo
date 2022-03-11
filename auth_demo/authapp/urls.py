e1e8a58ecd66c217fde1e1e15eaf41cbef8dd4e8
Advaith H L <niceadvaith@gmail.com>
2022-03-11 15:47:21 +0530

    Add login view

--- auth_demo/authapp/urls.py
+++ auth_demo/authapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='authapp-profile'),
+    path('', views.login, name='authapp-login'),
+    path('profile', views.profile, name='authapp-profile'),
 ]
--- auth_demo/authapp/views.py
+++ auth_demo/authapp/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
+
+def login(request):
+    return render(request, 'authapp/login.html')
+
 def profile(request):
     return render(request, 'authapp/profile.html')
