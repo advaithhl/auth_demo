ed3f6173cc0347c7191de7a924db0a7b6141e598
Advaith H L <niceadvaith@gmail.com>
2022-03-16 09:20:57 +0530

    Add basic login

--- auth_demo/authapp/urls.py
+++ auth_demo/authapp/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='authapp-login'),
+    path('', views.auth_login, name='authapp-login'),
     path('profile', views.profile, name='authapp-profile'),
 ]
--- auth_demo/authapp/views.py
+++ auth_demo/authapp/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login
 
 
-def login(request):
-    return render(request, 'authapp/login.html')
+def auth_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('authapp-profile')
+    else:
+        return render(request, 'authapp/login.html')
 
 def profile(request):
     return render(request, 'authapp/profile.html')
