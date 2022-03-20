547277b83c499ba4e6ab6ee96a9a60789c0b90ca
Advaith H L <niceadvaith@gmail.com>
2022-03-20 11:46:19 +0530

    Add seperate login for password

--- auth_demo/authapp/urls.py
+++ auth_demo/authapp/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
+other_login_urls = [
+    path('', views.auth_login, name='authapp-login'),
+    path('standard/', views.standard_login, name='standard-login'),
+    path('otp/', views.otp_login, name='otp-login'),
+]
+
 urlpatterns = [
-    path('login/', views.auth_login, name='authapp-login'),
+    path('login/', include(other_login_urls)),
     path('logout/', views.auth_logout, name='authapp-logout'),
     path('', views.profile, name='authapp-profile'),
 ]
--- auth_demo/authapp/views.py
+++ auth_demo/authapp/views.py
@@ -8,13 +8,31 @@ def auth_login(request):
         return redirect('authapp-profile')
     if request.method == 'POST':
         username = request.POST['username']
+        return render(request, 'authapp/passwordlogin.html', {'username': username})
+    else:
+        return render(request, 'authapp/login.html')
+
+
+def standard_login(request):
+    if request.user.is_authenticated:
+        return redirect('authapp-profile')
+    if request.method == 'POST':
+        username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('authapp-profile')
     else:
-        return render(request, 'authapp/login.html')
+        return redirect('authapp-login')
+
+
+def otp_login(request):
+    if request.user.is_authenticated:
+        return redirect('authapp-profile')
+    if request.method == 'POST':
+        username = request.POST['username']
+        return render(request, 'authapp/otplogin.html', {'username': 'username'})
 
 
 @login_required
